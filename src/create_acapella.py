0f855eee2a40aba695163fd586109086736fd81e
Marlon Flügge <marlonfl@fb3.uni-bremen.de>
2017-03-08 20:12:38 +0100

    push wip to continue working on pc

--- src/create_acapella.py
+++ src/create_acapella.py
@@ -1,9 +1,16 @@
 import numpy as np
 import scipy.io.wavfile as scw
+import sys
 
 winLength = 2048
 overlap = 4
 
+def remove_zeros(audio):
+    index = 0
+    while abs(audio[index][0]) + abs(audio[index][0]) < 8:
+        index += 1
+
+    return audio[index:]
 
 def stft(x, fftsize, overlap):
     """Returns short time fourier transform of a signal x
@@ -31,3 +38,12 @@ if __name__ == "__main__":
     orig_path = sys.argv[1]
     instr_path = sys.argv[2]
     fn = sys.argv[3]
+
+    orig_raw = scw.read(orig_path)
+    instr_raw = scw.read(instr_path)
+
+    print ("\norig cleaned")
+    print (remove_zeros(orig_raw[1])[0:30])
+
+    print ("\ninstr cleaned")
+    print (remove_zeros(instr_raw[1])[0:30])
